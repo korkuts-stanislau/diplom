17520864b19d7e9a55aa304b09e8cdd9d0525788
Korkuts Stanislau <stkorkuts@gmail.com>
2022-01-20 01:28:34 +0300

    added angular code with authorization

---
+++ Magik2.0/ui/src/services/routing/routing.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RoutingService {
+  private currentPage:string = "homepage";
+
+  constructor() {}
+
+  routeToHomepage() {
+    this.currentPage = "homepage";
+  }
+
+  routeToProjects() {
+    this.currentPage = "projects";
+  }
+
+  routeToProfile() {
+    this.currentPage = "profile";
+  }
+
+  isHomepage():boolean {
+    return this.currentPage == "homepage";
+  }
+
+  isProjects():boolean {
+    return this.currentPage == "projects";
+  }
+
+  isProfile():boolean {
+    return this.currentPage == "profile";
+  }
+}
