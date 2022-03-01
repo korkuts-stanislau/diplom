0da648b0b4477ac3ff369899cece377a837adaa7
Stanislau <skorkuts@gmail.com>
2022-03-01 17:39:06 +0300

    feat: tests to auth service added.

--- Magik2.0/tests/Auth/AuthServiceTests.cs
+++ Magik2.0/tests/Auth/AuthServiceTests.cs
@@ -7,6 +7,7 @@ using Common;
 using System.Threading.Tasks;
 using Auth.UIModels;
 using Auth.Models;
+using System;
 
 namespace Tests.Auth;
 
@@ -24,22 +25,132 @@ public class AuthServiceTests
     }
 
     [Fact]
-    public async Task SignUp_UserShouldSignUp()
+    public async Task SignUp_UserCanSignUp()
     {
         //Arrange
+        var email = "newuser@gmail.com";
+        var password = "strongpass123";
+
         AuthData data = new AuthData {
-            Email = "newuser@gmail.com",
-            Password = "strongpass123"
+            Email = email,
+            Password = password
         };
 
-        _accRepoMock.Setup(x => x.GetByEmailAsync(data.Email))
+        _accRepoMock.Setup(x => x.GetByEmailAsync(email))
             .ReturnsAsync(default(Account));
 
         //Act
         var account = await _as.SignUp(data);
+        
         //Assert
         Assert.Equal(data.Email, account.Email);
         Assert.Equal(Role.User, account.Roles[0]);
         Assert.Equal(1, account.Roles.Length);
+    }
+
+    [Fact]
+    public async Task SignUp_UserCanNotSignUp()
+    {
+        //Arrange
+        var email = "newuser@gmail.com";
+        var password = "strongpass123";
+
+        AuthData data = new AuthData {
+            Email = email,
+            Password = password
+        };
+        Account userAccount = new Account {
+            Email = email,
+            PasswordHash = _phs.Hash(password),
+            Roles = new Role[] {Role.User}
+        };
+
+        _accRepoMock.Setup(x => x.GetByEmailAsync(email))
+            .ReturnsAsync(userAccount);
+
+        //Act
+        Func<Task> signUp = async () => await _as.SignUp(data);
+
+        //Assert
+        await Assert.ThrowsAsync<Exception>(signUp);
+    }
+
+    [Fact]
+    public async Task SignUp_UserCanSignIn()
+    {
+        //Arrange
+        var email = "newuser@gmail.com";
+        var password = "strongpass123";
+
+        AuthData data = new AuthData {
+            Email = email,
+            Password = password
+        };
+        Account userAccount = new Account {
+            Email = email,
+            PasswordHash = _phs.Hash(password),
+            Roles = new Role[] {Role.User}
+        };
+
+        _accRepoMock.Setup(x => x.GetByEmailAsync(email))
+            .ReturnsAsync(userAccount);
+
+        //Act
+        var account = await _as.SignIn(data);
+        
+        //Assert
+        Assert.Equal(account.Email, userAccount.Email);
+        Assert.Equal(account.Roles[0], userAccount.Roles[0]);
+        Assert.Equal(account.PasswordHash, userAccount.PasswordHash);
+    }
+
+    [Fact]
+    public async Task SignUp_UserCanNotSignIn_WrongPassword()
+    {
+        //Arrange
+        var email = "newuser@gmail.com";
+        var password = "strongpass123";
+        var wrongPassword = "wrongPass";
+
+        AuthData data = new AuthData {
+            Email = email,
+            Password = wrongPassword
+        };
+        Account userAccount = new Account {
+            Email = email,
+            PasswordHash = _phs.Hash(password),
+            Roles = new Role[] {Role.User}
+        };
+
+        _accRepoMock.Setup(x => x.GetByEmailAsync(email))
+            .ReturnsAsync(userAccount);
+
+        //Act
+        Func<Task> signIn = async() => await _as.SignIn(data);
+        
+        //Assert
+        await Assert.ThrowsAsync<Exception>(signIn);
+    }
+
+    [Fact]
+    public async Task SignUp_UserCanNotSignIn_WrongEmail()
+    {
+        //Arrange
+        var wrongEmail = "wrong.email@wr.com";
+        var password = "strongpass123";
+
+        AuthData data = new AuthData {
+            Email = wrongEmail,
+            Password = password
+        };
+
+        _accRepoMock.Setup(x => x.GetByEmailAsync(wrongEmail))
+            .ReturnsAsync(default(Account));
+
+        //Act
+        Func<Task> signIn = async() => await _as.SignIn(data);
+        
+        //Assert
+        await Assert.ThrowsAsync<Exception>(signIn);
     }
 }
--- Magik2.0/tests/Auth/PasswordHasherServiceTests.cs
+++ Magik2.0/tests/Auth/PasswordHasherServiceTests.cs
@@ -14,13 +14,17 @@ public class PasswordHasherServiceTests
     }
 
     [Fact]
-    public async Task Hash_Verify_SamePassword()
+    public async Task Hash_Verify()
     {
         //Arrange
+        var password = "VeryStrongPassword123";
+        var wrongPassword = "WrongPassword";
 
         //Act
+        var hash = _phs.Hash(password);
 
         //Assert
-
+        Assert.True(_phs.Verify(password, hash));
+        Assert.False(_phs.Verify(wrongPassword, hash));
     }
 }
