15048f3fb23e4167bb0d82743e04c280ee5e0b4b
Stanislau Korkuts <stkorkuts@gmail.com>
2022-04-19 23:58:53 +0300

    finished work on report

--- Magik2.0/auth/Program.cs
+++ Magik2.0/auth/Program.cs
@@ -1,14 +1,3 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Server.Kestrel.Core;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Threading.Tasks;
-
 namespace Auth
 {
     public class Program
