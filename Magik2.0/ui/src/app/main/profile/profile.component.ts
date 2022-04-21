18b7a834f53dd82549ac80b4c4bd18fd01cd483f
Stanislau Korkuts <stkorkuts@gmail.com>
2022-04-21 21:35:20 +0300

    added info to report

--- Magik2.0/ui/src/app/main/profile/profile.component.ts
+++ Magik2.0/ui/src/app/main/profile/profile.component.ts
@@ -3,6 +3,7 @@ import {ProfilesService} from "../../../services/profile/profiles.service";
 import {Profile} from "../../../models/resource/profile";
 import {DomSanitizer, SafeResourceUrl} from "@angular/platform-browser";
 import { ProfileRoutingService } from 'src/services/routing/profile-routing.service';
+import { AuthService } from 'src/services/auth/auth.service';
 
 @Component({
   selector: 'app-profile',
@@ -15,7 +16,8 @@ export class ProfileComponent implements OnInit {
 
   constructor(private profileService: ProfilesService,
               private sanitizer: DomSanitizer,
-              public profileRoutingService: ProfileRoutingService) { }
+              public profileRoutingService: ProfileRoutingService,
+              private authService: AuthService) { }
 
   ngOnInit(): void {
     this.getProfile();
@@ -34,5 +36,11 @@ export class ProfileComponent implements OnInit {
   getPictureSource() {
     this.currentProfileImageUrl = this.sanitizer.bypassSecurityTrustResourceUrl(
       `data:image/png;base64, ${this.currentProfile?.picture}`);
+  }
+
+  signOut() {
+    if(confirm("Вы точно хотите выйти?")) {
+      this.authService.signOut();
+    }
   }
 }
